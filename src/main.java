3d3aa59345680c3ab7244b779f0aeb630220a585
TRUNGHIEU <trunghieu@TRUNGHIEUs-MacBook-Air.local>
2021-12-23 21:03:31 +0700

    Thêm tính năng 4 (Without GUI)

--- src/main.java
+++ src/main.java
@@ -8,8 +8,8 @@ import java.util.*;
  * Description: ...
  */
 public class main {
-    // Chức năng tìm kiếm theo slang word.
     public static HashMap<String,String> slangword = new HashMap<>();
+    public static ArrayList<String> history = new ArrayList<>();
 
     public static void ReadFile(){
         try {
@@ -27,33 +27,60 @@ public class main {
             System.out.println("wrong here!!!");
         }
     }
+
     // tính năng 1
     public static String SearchDefinition(String slang){
-        //System.out.println(slangword.get(slang));
         StringBuilder tmp = new StringBuilder();
         for (Map.Entry<String, String> entry: slangword.entrySet())
-            if (entry.getKey().contains(slang))
+            if (entry.getKey().contains(slang)) {
                 tmp.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
+                history.add(entry.getKey() + " " + entry.getValue());
+            }
         return tmp.toString();
     }
 
     // tính năng 2
-    public static String getKey(String value)
-    {
+    public static String SearchSlang(String value) {
         StringBuilder tmp = new StringBuilder();
         for (Map.Entry<String, String> entry: slangword.entrySet())
-            if (entry.getValue().toLowerCase(Locale.ROOT).contains(value))
+            if (entry.getValue().toLowerCase(Locale.ROOT).contains(value)) {
                 tmp.append(entry.getKey()).append(" ").append(entry.getValue()).append("\n");
+                history.add(entry.getKey() + " " + entry.getValue());
+            }
         return tmp.toString();
+    }
+
+    // tính năng 4
+    public static void AddSlang(String slang, String value){
+        String check = "";
+        check = slangword.get(slang);
+        // thông báo bị trùng key, overwrite hay duplicate hay khỏi làm
+
+        if(!Objects.equals(check, "")){ // có slang trùng
+            // overwrite
+            //slangword.put(slang, value);
+
+            // duplicate
+            String old = slangword.get(slang);
+            slangword.put(slang,old + " | " + value);
+        }
+        else { // không có slang trùng
+            System.out.println("No slang");
+            slangword.put(slang, value);
+        }
+
+
     }
 
     public static void main(String[] argv){
         ReadFile();
 
         long start = System.currentTimeMillis();
-        System.out.println(SearchDefinition("#1"));
+        AddSlang("$","tien viet");
         long elapsedTimeMillis = System.currentTimeMillis()-start;
         System.out.println(elapsedTimeMillis + " ms");
+
+        System.out.println(SearchDefinition("$"));
 
     }
 
